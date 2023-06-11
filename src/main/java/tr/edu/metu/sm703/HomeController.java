5c348852b1fe083e03f60424dd37770d0fc3d204
aliyolo <aliberkcan.boylu@gmail.com>
2023-06-11 15:07:22 +0300

    Hotfix Test-4

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 @Controller
 public class HomeController {
-//Hot Fix Test Commit-3
+//Hot Fix Test Commit-4
     @Get
     public Map<String, Object> index() {
         return Collections.singletonMap("message", "Hello World from prod");
