bc94a23857ab8b142c9569c7022456b8d93de9b2
aliyolo <aliberkcan.boylu@gmail.com>
2023-06-14 18:05:51 +0300

    codeql test-2

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @Get
     public Map<String, Object> index() {
 
-        String username = null;
+        String username = "user";
         String password = "123456";
 
         // Simulating a potential security vulnerability
